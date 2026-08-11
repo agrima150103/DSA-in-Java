325bbb10b731c62e9d727fed76f2e1befad38f02
Agrima Saxena <agrimalc@gmail.com>
2026-08-12 01:37:25 +0530

    Codes

---
+++ Operators/bitwiseop.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class bitwiseop {
+    public static void main(String args[]) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter the first number");
+        int a=sc.nextInt();
+        System.out.println("enter the second number");
+        int b=sc.nextInt();
+        
+        System.out.println("Bitwise AND: "+(a&b));
+        System.out.println("Bitwise OR: "+(a|b));
+        System.out.println("Bitwise XOR: "+(a^b));
+        System.out.println("Bitwise NOT of first number: "+(~a));
+        System.out.println("Bitwise NOT of second number: "+(~b));
+    }
+}
