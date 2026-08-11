1eebb4688df41bd589d8a5ac52c640cf8e25a1de
Agrima Saxena <agrimalc@gmail.com>
2026-08-12 01:38:33 +0530

    Codes

--- Operators/bitwiseop1.java
+++ Operators/bitwiseop1.java
@@ -1,5 +1,5 @@
 import java.util.*;
-public class bitwiseop {
+public class bitwiseop1 {
     public static void main(String args[]) {
         Scanner sc=new Scanner(System.in);
         System.out.println("enter the first number");
---
+++ Operators/bitwiseop2.java
@@ -0,0 +1,5 @@
+package Operators;
+
+public class bitwiseop2 {
+    
+}
