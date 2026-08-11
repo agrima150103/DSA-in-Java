ddc2153ba8e5b93f717b6c7886f4923ca551e2fb
Agrima Saxena <agrimalc@gmail.com>
2026-08-12 00:30:50 +0530

    Codes

---
+++ Strings/string3.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class string3 {
+    public static void main(String args[]) {
+       Scanner sc=new Scanner(System.in);
+       System.out.println("enter the string");
+       String str=sc.nextLine();
+       
+       for(int i=str.length()-1;i>=0;i--)
+       {
+           System.out.print(str.charAt(i));
+       }
+       }
+}
