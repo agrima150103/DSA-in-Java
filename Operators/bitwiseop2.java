1bbac7c30904c75329fbaf2dad780cee47c79e5e
Agrima Saxena <agrimalc@gmail.com>
2026-08-12 01:42:56 +0530

    Codes

--- Operators/bitwiseop2.java
+++ Operators/bitwiseop2.java
@@ -1,5 +1,13 @@
-package Operators;
-
+import java.util.*;
 public class bitwiseop2 {
-    
+    public static void main(String args[]) {
+        int n;
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter the number n");
+        n=sc.nextInt();
+        int bitmask=n<<2;
+        System.out.println("Bitmask after left shift by 2: "+bitmask);
+                  System.out.println("Bitmask AND n: "+(bitmask&n));
+}
+
 }
