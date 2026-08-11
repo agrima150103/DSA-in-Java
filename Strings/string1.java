f1f0361fe07c209c58aca74c0a045d80324c1af0
Agrima Saxena <agrimalc@gmail.com>
2026-08-12 00:17:13 +0530

    Codes

---
+++ Strings/string1.java
@@ -0,0 +1,10 @@
+import java.util.*;
+class string1
+{
+    public static void main(String args[])
+    {
+        StringBuilder str=new StringBuilder("Hello");
+        str=str.append(" Programming");
+        System.out.println(str);
+    }
+}
