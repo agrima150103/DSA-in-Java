2494cc7a697b7c535b8770441ba615a0af0acb0f
Agrima Saxena <agrimalc@gmail.com>
2026-08-13 01:23:58 +0530

    Codes

---
+++ Sorting/InsertionSort.java
@@ -0,0 +1,18 @@
+public class InsertionSort {
+    public static void main(String args[]) {
+        int j;
+        int arr[] = {7, 8, 3, 1, 2};
+        for (int i = 1; i < arr.length; i++) {
+            int current = arr[i];
+            
+            for(j = i - 1; j >= 0 && arr[j] > current; j--) {
+                arr[j + 1] = arr[j];
+            }
+            arr[j + 1] = current;
+        }
+        for (int i = 0; i < arr.length; i++) // to print entire new array after sorting
+        {
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
