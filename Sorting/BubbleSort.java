fc3a359d58df479296a094e10a1c788b4b96b3c9
Agrima Saxena <agrimalc@gmail.com>
2026-08-12 22:38:31 +0530

    Codes

--- Sorting/BubbleSort.java
+++ Sorting/BubbleSort.java
@@ -5,12 +5,22 @@ public class BubbleSort
     {
         int arr[]={7,8,3,1,2};
         for (int i = 0; i<arr.length-1; i++)
+        {
             for (int j = 0; j<arr.length-i-1; j++)
+            {
                 if (arr[j] > arr[j+1])
                 {
                     // swap arr[j+1] and arr[i]
                     int temp = arr[j];
                     arr[j] = arr[j+1];
+                    arr[j+1] = temp;
                  
                 }
-            }}
+            
+          
+        }
+    }  for (int i = 0; i < arr.length; i++) {
+        System.out.print(arr[i] + " ");
+    }
+}
+}
