dd362f3974eaf650c99f0877647ae3fcde4e1627
Agrima Saxena <agrimalc@gmail.com>
2026-08-12 22:46:08 +0530

    Codes

--- Sorting/SelectionSort.java
+++ Sorting/SelectionSort.java
@@ -1,3 +1,19 @@
 public class SelectionSort {
-    
+    public static void main(String args[]) {
+        int arr[] = {7, 8, 3, 1, 2};
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[i] > arr[j]) {
+                    //swap 
+                  int temp=arr[i];
+                   arr[i]=arr[j];
+                   arr[j]=temp;
+                }
+            }
+                  }
+        for (int i = 0; i < arr.length; i++) // to print entire new array after sorting
+        {
+            System.out.print(arr[i] + " ");
+        }
+    }
 }
